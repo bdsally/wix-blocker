90810cc4d6725f236633d369254431adc34d70a9
maximocampo <dev.maximocampo@gmail.com>
2024-02-13 17:14:58 -0600

    fixes

--- src/notice.js
+++ src/notice.js
@@ -11,11 +11,11 @@ function replaceTemplateStrings(template, data) {
 
 // Function to update the leaderboard visits by making a fetch call
 async function updateLeaderboardVisits(visitedSite) {
-  const url = "UPDATE_LEADERBOARD_VISITS_ENDPOINT"
+  const url = "https://dtdykmfxiqhxjwutnzkz.supabase.co/functions/v1/update-leaderboard-visits"
   const options = {
     body: JSON.stringify({ company_name: visitedSite }),
     headers: {
-      Authorization: "UPDATE_LEADERBOARD_VISITS_API_KEY",
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0ZHlrbWZ4aXFoeGp3dXRuemt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MzkwNDYsImV4cCI6MjAyMzQxNTA0Nn0.Ls1iXTyV9v9ki-zJAl7o795VO90w0jwzjXuxi5QM47o",
       "Content-Type": "application/json",
     },
     method: "POST",
@@ -36,24 +36,27 @@ async function updateLeaderboardVisits(visitedSite) {
 
 // Main function to handle the extension's page load logic
 function handlePageLoad() {
-  if (window.hasMadeFetchCall) {
-    return // Prevent duplicate execution
-  }
-  window.hasMadeFetchCall = true
-
   chrome.storage.local.get("notice", async function (result) {
     if (!result.notice) return
 
     const { visited_site: visitedSite } = result.notice
 
-    // Update leaderboard visits
-    const { visits } = await updateLeaderboardVisits(visitedSite)
 
     // Process and update the page content
     const body = document.getElementsByTagName("body")[0]
-    const updatedContent = replaceTemplateStrings(body.innerHTML, { visited_site: visitedSite, visits })
+    const updatedContent = replaceTemplateStrings(body.innerHTML, {
+      visited_site: visitedSite,
+      visits: localStorage.getItem(visitedSite) || 1,
+    })
     body.innerHTML = updatedContent
     body.style.display = "block"
+
+    // Update leaderboard visits
+    const { visits } = await updateLeaderboardVisits(visitedSite)
+    body.innerHTML = replaceTemplateStrings(body.innerHTML, { visited_site: visitedSite, visits })
+
+    // Update the local storage with the new visits count
+    localStorage.setItem(visitedSite, visits)
 
     // Set up the bypass link
     document.getElementById("bypass-link")?.addEventListener(
